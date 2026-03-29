608330cc0b59ab256f4d06765b1879b4a6c8fdf4
csab-csab <csabaszabo1211@gmail.com>
2026-03-29 22:11:48 +0100

    Made it so delivery points array assigns automatically at the start of every game

--- Assets/Scripts/Data Persistance/GameData/GameData.cs
+++ Assets/Scripts/Data Persistance/GameData/GameData.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 public class GameData
 {
     public int currentQuestProgress = 0;
-    public float playerMoney = 0;    
+    public float playerMoney = 0;  
+
+    public int deliveriesCompleted = 0;   
 }
